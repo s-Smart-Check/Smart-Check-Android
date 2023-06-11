c3ba6de5b85a8e30bbcad4bc858b99db6ef89ba4
BeamjunCho9 <107917980+BeamjunCho9@users.noreply.github.com>
2023-06-12 00:32:46 +0900

    스플래시 화면 추가

--- app/src/main/java/com/example/smartattendancecheckapp/model/response/StudentAttendanceRes.kt
+++ app/src/main/java/com/example/smartattendancecheckapp/model/response/StudentAttendanceRes.kt
@@ -2,5 +2,6 @@ package com.example.smartattendancecheckapp.model.response
 
 data class StudentAttendanceRes(
     var className: String,
+    var professor: String,
     var attendance: Boolean
 )
--- app/src/main/java/com/example/smartattendancecheckapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/smartattendancecheckapp/ui/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.example.smartattendancecheckapp.ui
 
+import android.animation.ObjectAnimator
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
+import android.view.animation.AnticipateInterpolator
+import androidx.annotation.RequiresApi
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.databinding.DataBindingUtil
 import com.example.smartattendancecheckapp.R
 import com.example.smartattendancecheckapp.databinding.ActivityMainBinding
@@ -11,9 +19,30 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        splashScreen.setOnExitAnimationListener { splashScreenView ->
+            // Create your custom animation.
+            val slideUp = ObjectAnimator.ofFloat(
+                splashScreenView,
+                View.TRANSLATION_Y,
+                0f,
+                -splashScreenView.height.toFloat()
+            )
+            slideUp.interpolator = AnticipateInterpolator()
+            slideUp.duration = 3000L
+
+            // Call SplashScreenView.remove at the end of your custom animation.
+            slideUp.doOnEnd { splashScreenView.remove() }
+
+            // Run your animation.
+            slideUp.start()
+        }
 
         // 회원 가입 버튼 클릭
         binding.btnJoin.setOnClickListener {
