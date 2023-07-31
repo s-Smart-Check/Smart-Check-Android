c8cff88baf87c904009159e53bb5323c72280d0a
BeamjunCho9 <107917980+BeamjunCho9@users.noreply.github.com>
2023-07-31 17:51:49 +0900

    캘린더 뷰 추가, 날짜 선택 시 해당 날짜 받는 로직 추가

--- app/src/main/java/com/example/smartattendancecheckapp/ui/CalendarCheckFragment.kt
+++ app/src/main/java/com/example/smartattendancecheckapp/ui/CalendarCheckFragment.kt
@@ -1,6 +1,7 @@
 package com.example.smartattendancecheckapp.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -24,5 +25,12 @@ class CalendarCheckFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.lifecycleOwner = viewLifecycleOwner
+
+        binding.apply {
+            calendar.setOnDateChangeListener { _, year, month, dayOfMonth ->
+
+                Log.d("캘린더 뷰 태그", "${year}년 ${month}월 ${dayOfMonth}일")
+            }
+        }
     }
 }
