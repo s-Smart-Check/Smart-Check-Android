97d0946e02a9bf7bfa03947936ecd9424440ef60
BeamjunCho9 <107917980+BeamjunCho9@users.noreply.github.com>
2023-10-25 20:23:12 +0900

    시작 화면을 로그인 화면으로 수정, ui 수정, 앱 아이콘 설정

--- app/src/main/java/com/example/smartattendancecheckapp/presentation/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/smartattendancecheckapp/presentation/ui/login/LoginActivity.kt
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.databinding.DataBindingUtil
 import com.example.smartattendancecheckapp.R
 import com.example.smartattendancecheckapp.databinding.ActivityLoginBinding
 import com.example.smartattendancecheckapp.domain.model.request.LoginData
 import com.example.smartattendancecheckapp.presentation.ui.main2.MainActivity2
+import com.example.smartattendancecheckapp.presentation.ui.signup.SignUpActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 lateinit var usrNum : String
@@ -22,9 +24,17 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        installSplashScreen()
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
 
         val viewModel: LoginViewModel by viewModels()
+
+        // 회원 가입 버튼 클릭
+        binding.tvJoin.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
 
         // 로그인 버튼 클릭 시
         binding.btnLoginLogin.setOnClickListener {
@@ -70,37 +80,6 @@ class LoginActivity : AppCompatActivity() {
                         else -> {}
                     }
                 }
-
-//                retrofitService.requestLogin(
-//                    LoginData(
-//                    binding.edtLoginStudentNum.text.toString(),
-//                    binding.edtLoginPassword.text.toString()
-//                )
-//                ).enqueue(object : retrofit2.Callback<LoginRes> {
-//                    // 정상적으로 응답이 온 경우
-//                    override fun onResponse(call: Call<LoginRes>, response: Response<LoginRes>) {
-//                        if (response.isSuccessful){
-//                            when(response.code()) {
-//                                200 -> {
-//                                    usrNum = binding.edtLoginStudentNum.text.toString()
-//                                    Toast.makeText(this@LoginActivity, "로그인 성공", Toast.LENGTH_SHORT).show()
-//                                    val intent = Intent(this@LoginActivity, MainActivity2::class.java)
-//                                    startActivity(intent)
-//                                }
-//                                400 -> {
-//                                    if (response.message() == "비밀번호가 맞지 않습니다.")
-//                                        Toast.makeText(this@LoginActivity, "비번", Toast.LENGTH_SHORT).show()
-//                                    else if (response.message() == "존재하지 않는 아이디입니다.")
-//                                        Toast.makeText(this@LoginActivity, "아이디", Toast.LENGTH_SHORT).show()
-//                                }
-//                            }
-//                        }
-//                    }
-//                    // 통신에 실패한 경우
-//                    override fun onFailure(call: Call<LoginRes>, t: Throwable) {
-//                        Toast.makeText(this@LoginActivity, "로그인 실패", Toast.LENGTH_SHORT).show()
-//                    }
-//                })
             }
             // 입력 값이 없는 경우
             else { Toast.makeText(this@LoginActivity, "아이디, 비밀번호 입력하세요", Toast.LENGTH_SHORT).show() }
